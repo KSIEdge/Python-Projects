fbe78e0adb9c7b4958bc63ce7e07a1b896b12ef3
Nick <46254507+KSIEdge@users.noreply.github.com>
2024-01-10 16:15:40 -0700

    Create Variable Manipulation.py

---
+++ Variable Manipulation.py
@@ -0,0 +1,80 @@
+#1
+# Assign the `device_id` variable to the device ID that only specified users can access
+device_id = "72e08x0"
+# Display `device_id`
+print(device_id)
+
+#2
+# Assign the `device_id` variable to the device ID that only specified users can access
+device_id = "72e08x0"
+# Assign `device_id_type` to the data type of `device_id`
+device_id_type = device_id
+# Display `device_id_type`
+print(device_id_type)
+
+#3
+# Assign `username_list` to the list of usernames who are allowed to access the device
+username_list = ["madebowa", "jnguyen", "tbecker", "nhersh", "nhersh"]
+# Display `username_list`
+print(username_list)
+
+#4
+# Assign `username_list` to the list of usernames who are allowed to access the device
+username_list = ["madebowa", "jnguyen", "tbecker", "nhersh", "redwards"]
+# Assign `username_list_type` to the data type of `username_list`
+username_list_type = username_list
+# Display `username_list_type`
+print(username_list_type)
+
+#5
+# Assign `username_list` to the list of usernames who are allowed to access the device
+username_list = ["madebowa", "jnguyen", "tbecker", "nhersh", "redwards"]
+# Display `username_list`
+print(username_list)
+# assign `username_list` to the updated list of usernames who are allowed to access the device
+username_list = ["madebowa", "jnguyen", "tbecker", "nhersh", "redwards", "lpope"]
+# Display `username_list`
+print(username_list)
+
+#6
+# Assign `max_logins` to the value 3
+max_Logins = 3
+# Assign `max_logins_type` to the data type of `max_logins`
+max_Logins_type = max_Logins
+# Display `max_logins_type`
+print(max_Logins_type)
+
+#7
+# Assign `login_attempts` to the value 2
+Login_attempts = 2
+# Assign `login_attempts_type` to the data type of `login_attempts`
+Login_attempts_type = Login_attempts
+# Display `login_attempts_type`
+print(Login_attempts_type)
+
+#8
+# Assign `max_logins` to the value 3
+max_logins = 3
+# Assign `login_attempts` to the value 2
+login_attempts = 2
+# Determine whether the current number of login attempts a user has made is less than or equal to the maximum number of login attempts allowed,
+# and display the resulting Boolean value
+print(Login_attempts <= max_logins)
+
+#9
+# Assign `max_logins` to the value 3
+max_logins = 3
+# Assign `login_attempts` to a specific value
+login_attempts = 10
+# Determine whether the current number of login attempts a user has made is less than or equal to the maximum number of login attempts allowed,
+# and display the resulting Boolean value
+print(login_attempts <= max_logins)
+
+#10
+# Assign `login_status` to the Boolean value False
+login_status = False
+# Assign `login_status_type` to the data type of `login_status`
+login_status_type = login_status
+# Removed the default type() bool as it will return an error in the lab
+# Display `login_status_type`
+print(login_status_type)
