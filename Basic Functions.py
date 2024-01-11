f4e1289a0af8bf00549041003c9b0c7d623e9ef4
Nick <46254507+KSIEdge@users.noreply.github.com>
2024-01-10 19:11:35 -0700

    Create Basic Functions.py

---
+++ Basic Functions.py
@@ -0,0 +1,20 @@
+Conditional statements
+The following keywords and operators are used in conditional statements.
+
+== != < > <= >= and or not
+
+if
+if value in list:
+elif
+else
+
+Iterative statements
+The following keywords are used in iterative statements.
+
+for value in list:
+for i in list:
+for i in range(int):
+
+while value < int:
+break
+continue
