94232507310f0c013fcbd0c52afb95385a6ec07c
Nick <46254507+KSIEdge@users.noreply.github.com>
2024-01-10 19:21:41 -0700

    Update Basic Functions.py

--- Basic Functions.py
+++ Basic Functions.py
@@ -1,5 +1,6 @@
 Conditional statements
 The following keywords and operators are used in conditional statements.
+#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 == != < > <= >= and or not
 
@@ -11,19 +12,23 @@ else
 Iterative statements
 The following keywords are used in iterative statements.
 
+For If Else Statements
 for value in list:
 for i in list:
 for i in range(int):
 
-""" EXAMPLE """
+for i in ip_addresses:
+	if i in allow_list:
+		print(i+"IP address is allowed")
+	else:
+		print(i+"IP address is not allowed. Further investigation of login activity required")
+		break
 
-
-
+While Statements
 while value < int:
 break
 continue
 
-""" EXAMPLE """
 while i <= 5150:
     print (i)
     i = i + 5
