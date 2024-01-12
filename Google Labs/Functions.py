5039029482ea05edf639d8852b2dd6c8595e0277
Nick <46254507+KSIEdge@users.noreply.github.com>
2024-01-12 15:23:33 -0700

    Create Functions.py

---
+++ Google Labs/Functions.py
@@ -0,0 +1,52 @@
+#1
+# Define a function named `alert()` 
+def alert():
+    print("Potential security issue. Investigate further.")
+  
+#2
+# Define a function named `alert()` 
+def alert():
+    print("Potential security issue. Investigate further.")
+# Call the `alert()` function
+alert()
+
+#3
+# Define a function named `list_to_string()`
+def list_to_string():
+  # Store the list of approved usernames in a variable named `username_list`
+  username_list = ["elarson", "bmoreno", "tshah", "sgilmore", "eraab", "gesparza", "alevitsk", "wjaffrey"]
+  # Write a for loop that iterates through the elements of `username_list` and displays each element
+  for i in username_list:
+    print(i)
+# Call the `list_to_string()` function
+list_to_string()
+
+#4
+# Define a function named `list_to_string()`
+def list_to_string():
+  # Store the list of approved usernames in a variable named `username_list`
+  username_list = ["elarson", "bmoreno", "tshah", "sgilmore", "eraab", "gesparza", "alevitsk", "wjaffrey"]
+  # Assign `sum_variable` to an empty string
+  sum_variable = ""
+  # Write a for loop that iterates through the elements of `username_list` and displays each element
+  for i in username_list:
+    sum_variable = sum_variable + i
+  # Display the value of `sum_variable`
+    print(sum_variable)
+# Call the `list_to_string()` function
+list_to_string()
+
+#5
+# Define a function named `list_to_string()`
+def list_to_string():
+  # Store the list of approved usernames in a variable named `username_list`
+  username_list = ["elarson", "bmoreno", "tshah", "sgilmore", "eraab", "gesparza", "alevitsk", "wjaffrey"]
+  # Assign `sum_variable` to an empty string
+  sum_variable = ""
+  # Write a for loop that iterates through the elements of `username_list` and displays each element
+  for i in username_list:
+    sum_variable = sum_variable + i + ", "
+  # Display the value of `sum_variable`
+  print(sum_variable)
+# Call the `list_to_string()` function
+list_to_string()
